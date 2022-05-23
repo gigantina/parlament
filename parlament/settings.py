78bb653968ec0a7e8b708d2449a57753fa6f1a5d
gigantina <gigantina.ru@yandex.ru>
2022-05-23 22:12:32 +0300

    static

--- parlament/settings.py
+++ parlament/settings.py
@@ -135,7 +135,7 @@ ACCOUNT_LOGOUT_ON_GET = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
