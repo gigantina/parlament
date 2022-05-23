cd14bb46a6ec091465160603e4ffa8adc41742f1
gigantina <gigantina.ru@yandex.ru>
2022-05-23 23:51:36 +0300

    Heroku

--- parlament/settings.py
+++ parlament/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +153,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [ 
     BASE_DIR / "static",
 ]
+
+django_heroku.settings(locals())
